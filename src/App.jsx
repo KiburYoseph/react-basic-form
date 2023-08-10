6a792e5b41914f4d7679c59ae92982ab6f7b7b0d
KiburYoseph <kiburstgr@gmail.com>
2023-08-10 09:19:27 +0300

    Files Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+  const App = () => {
+    return (
+      <>
+        <main>
+          <form>
+            <input type='text' placeholder='First Name'></input>
+            <input type='text' placeholder='Last Name'></input>
+            <input type="email" placeholder='Email'></input>
+            <button>Register</button>
+          </form>
+        </main>
+      </>
+    )
+  }
+
+export default App
