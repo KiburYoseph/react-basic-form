2afddce67785433a3ffc54a2f158edb9de92501a
KiburYoseph <kiburstgr@gmail.com>
2023-08-10 09:59:35 +0300

    Handling Submit

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,30 @@ import './App.css'
       email:"",
     })
 
+    const [sumbitted, setSubmitted] = useState(false)
+
+    const handleFirstName = (event) => {
+      setValues({...values, firstName: event.target.value})
+    }
+
+    const handleLastName = (event) => {
+      setValues({...values, lastName: event.target.value})
+    }
+
+    const handleEmail = (event) => {
+      setValues({...values, email: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+    }
+
 
     return (
       <>
         <main>
-          <div className='success'>Success! Thank you for registering.</div>
-          <form>
+          {sumbitted ? <div className='success'>Success! Thank you for registering.</div> : null}
+          <form onClick={handleSubmit}>
             <input type='text' placeholder='First Name' value={values.firstName} onChange={handleFirstName}></input>
             <p className='error'>Please enter a first name</p>
             <input type='text' placeholder='Last Name' value={values.lastName} onChange={handleLastName}></input>
