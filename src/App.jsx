d46941924debbd15df43c30483659d4d29738650
KiburYoseph <kiburstgr@gmail.com>
2023-08-10 10:35:48 +0300

    Validation Complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import './App.css'
       email:"",
     })
 
-    const [sumbitted, setSubmitted] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const [valid, setValid] = useState(true)
+
 
     const handleFirstName = (event) => {
       setValues({...values, firstName: event.target.value})
@@ -25,21 +27,31 @@ import './App.css'
     }
 
     const handleSubmit = (event) => {
-      event.preventDefault()
-    }
+      event.preventDefault();
 
+      setSubmitted(true)
+
+      if (values.firstName != "" && values.lastName != "" && values.email != ""){
+        setValid(false)
+      }
+      else {
+        setValid(true)
+      }
+    }
+    
+    
 
     return (
       <>
         <main>
-          {sumbitted ? <div className='success'>Success! Thank you for registering.</div> : null}
+          {submitted && !valid ? <div className='success'>Success! Thank you for registering.</div> : null}
           <form onClick={handleSubmit}>
             <input type='text' placeholder='First Name' value={values.firstName} onChange={handleFirstName}></input>
-            <p className='error'>Please enter a first name</p>
+          {submitted && !values.firstName ? <p className='error'>Please enter a first name</p> : null}  
             <input type='text' placeholder='Last Name' value={values.lastName} onChange={handleLastName}></input>
-            <p className='error'>Please enter a last name</p>
+          {submitted && !values.lastName ? <p className='error'>Please enter a last name</p> : null} 
             <input type="email" placeholder='Email' value={values.email} onChange={handleEmail}></input>
-            <p className='error'>Please enter an email address.</p>
+          {submitted && !values.email ? <p className='error'>Please enter an email</p> : null} 
             <button>Register</button>
           </form>
         </main>
